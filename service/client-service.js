5bda425dfe81ff221a988f588de09849e144da5b
natalyq1 <onatylili@gmail.com>
2023-03-20 21:36:02 -0500

    creating client.controller

--- service/client-service.js
+++ service/client-service.js
@@ -1,47 +1,8 @@
-const crearNuevaLinea = (nombre, email) => {
-    const linea = document.createElement("tr")
-  const contenido= `
-    <td class="td" data-td>
-      ${nombre}
-    </td>
-    <td>${email}</td>
-    <td>
-      <ul class="table__button-control">
-        <li>
-          <a
-            href="../screens/editar_cliente.html"
-            class="simple-button simple-button--edit"
-          >
-            Editar
-          </a>
-        </li>
-        <li>
-          <button class="simple-button simple-button--delete" type="button">
-            Eliminar
-          </button>
-        </li>
-      </ul>
-    </td>
-  `;
-  linea.innerHTML = contenido
-  return linea
-};
-
-const table = document.querySelector("[data-table]")
 
 //FETCH API
 const listaClientes = () => fetch("http://localhost:3000/perfil").then(respuesta => respuesta.json()
     )
-  
-  //console.log(listaClientes());
-  listaClientes().then((data) => {
-    //console.log(data);
-      data.forEach((perfil) => {
-        const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
-        table.appendChild(nuevaLinea);
-      });
-    })
-    .catch((error) => alert("Ocurrió un error"));
-  
 
-  
+export const clientServices = {
+    listaClientes
+}
