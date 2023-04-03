18ded5e3abe15d35d28f9135ca6f770adbe85627
natalyq1 <onatylili@gmail.com>
2023-04-02 22:23:57 -0500

    create a new client and save it on db.json and show them on website

--- service/client-service.js
+++ service/client-service.js
@@ -1,8 +1,22 @@
-
 //FETCH API
-const listaClientes = () => fetch("http://localhost:3000/perfil").then(respuesta => respuesta.json()
-    )
+const listaClientes = () =>
+  fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
+
+const crearCliente = (nombre, email) => {
+  return fetch("http://localhost:3000/perfil", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      nombre,
+      email,
+      id: uuid.v4()
+    })
+  })
+}
 
 export const clientServices = {
-    listaClientes
-}
+  listaClientes,
+  crearCliente
+};
