6b46477ba8eaceec3813b7a1b4dd9433164424b0
natalyq1 <onatylili@gmail.com>
2023-04-04 21:01:13 -0500

    Await- async, try-catch

--- controllers/actualizar.controller.js
+++ controllers/actualizar.controller.js
@@ -2,7 +2,7 @@ import { clientServices } from "../service/client-service.js";
 
 const formulario = document.querySelector("[data-form]");
 
-const obtenerInformacion = () => {
+const obtenerInformacion = async () => {
   const url = new URL(window.location);
   const id = url.searchParams.get("id");
 
@@ -12,10 +12,18 @@ const obtenerInformacion = () => {
   const nombre = document.querySelector("[data-nombre]");
   const email = document.querySelector("[data-email]");
 
-  clientServices.detalleCliente(id).then((perfil) => {
-    nombre.value = perfil.nombre;
-    email.value = perfil.email;
-  });
+  try {
+    const perfil = await clientServices.detalleCliente(id);
+    if (perfil.nombre && perfil.email) {
+      nombre.value = perfil.nombre;
+      email.value = perfil.email;
+    } else {
+      throw new Error();
+    }
+  } catch (error) {
+    //console.log("Catch error- ", error);
+    window.location.href ='/screens/error.html'
+  }
 };
 obtenerInformacion();
 
@@ -29,6 +37,6 @@ formulario.addEventListener("submit", (evento) => {
 
   console.log(nombre, "-", email);
   clientServices.actualizarCliente(nombre, email, id).then(() => {
-    window.location.href = '/screens/edicion_concluida.html'
-  })
+    window.location.href = "/screens/edicion_concluida.html";
+  });
 });
